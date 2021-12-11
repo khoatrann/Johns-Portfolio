9cfbf05d7371299030bb68f625fd7bdd0659a78b
Aden <adeziio@yahoo.com>
2021-12-11 05:13:49 -0500

    major update add resume

--- src/app/About-Me/about-me.component.ts
+++ src/app/About-Me/about-me.component.ts
@@ -6,8 +6,7 @@ import { MatTableDataSource } from '@angular/material/table';
 
 
 export interface attributes {
-    name: string;
-    details: string[];
+    value: string;
 }
 
 @Component({
@@ -18,11 +17,58 @@ export interface attributes {
 
 
 export class AboutMeComponent {
-    displayedColumns: string[] = ['name', 'details'];
-    DATA: attributes[] = [
-        { name: 'George Mason University', details: ['Courses: MIS303 & BUS 303', 'Enrolled in Business Information Systems and Develop Special Skills'] }
+    educationDisplayedColumns: string[] = ['value'];
+    educationObject: attributes[] = [
+        {
+            value: 'George Mason University'
+        },
+        {
+            value: 'Northern Virginia Community College'
+        }
     ];
-    dataSource = new MatTableDataSource(this.DATA);
+    educationDataSource = new MatTableDataSource(this.educationObject);
+
+    gpaDisplayedColumns: string[] = ['value'];
+    gpaObject: attributes[] = [
+        {
+            value: '?'
+        },
+        {
+            value: '3.5 (cum louder)'
+        }
+    ];
+    gpaDataSource = new MatTableDataSource(this.gpaObject);
+
+    jobDisplayedColumns: string[] = ['value'];
+    jobObject: attributes[] = [
+        {
+            value: 'Assist with basic needs (walking, eating, toileting, etc)'
+        },
+        {
+            value: 'Monitor medications for daily consumption'
+        },
+        {
+            value: 'Support with housekeeping (washing dishes, cleaning laundry, vacuuming floors)'
+        },
+        {
+            value: 'Follow exercise plan (standing up from seat multiple times, arm circles/raises)'
+        },
+        {
+            value: 'Translate between English and Vietnamese to help her talk with native English speakers'
+        }
+    ];
+    jobDataSource = new MatTableDataSource(this.jobObject);
+
+    skillsDisplayedColumns: string[] = ['value'];
+    skillsObject: attributes[] = [
+        {
+            value: 'Skills: Word; Excel; PowerPoint; Organization; 60+ WPM Typist; Fluent in Vietnamese; Windows; MacOS'
+        },
+        {
+            value: 'Interests: Gaming; Basketball; Spending time with friends; Bible; Stocks; Computer Hardware; Anime; Netflix; Movies; Crypto'
+        }
+    ];
+    skillsDataSource = new MatTableDataSource(this.skillsObject);
 
 
     constructor(public matDialog: MatDialog) {
